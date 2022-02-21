aca17881c14d85d3801b8d91ed50d7425b5db06b
Holarjeeday <Holarjeeday@gmail.com>
2022-02-21 14:37:14 +0000

    changes done on 17.02

--- sample.py
+++ sample.py
@@ -1,5 +1,5 @@
  #this is a day i will always remember 
 
-print (" happy learning  new concepts added ")
+print (" learning github ")
  #GETTING TO KNOW GITHUB  ok
 print (" fouth line addded and will add fifth line too ")

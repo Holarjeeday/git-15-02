e237e681845335bee28086aad223118d3a2db82a
Holarjeeday <Holarjeeday@gmail.com>
2022-02-22 00:30:43 +0000

    using git revert
    This reverts commit aca17881c14d85d3801b8d91ed50d7425b5db06b.

--- sample.py
+++ sample.py
@@ -1,5 +1,5 @@
  #this is a day i will always remember 
 
-print (" learning github ")
+print (" happy learning  new concepts added ")
  #GETTING TO KNOW GITHUB  ok
 print (" fouth line addded and will add fifth line too ")
